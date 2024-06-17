32abec0ca3fe55b99e724f7df6891b1b8dc9d7f4
sujanyaD <soujanya.gh@gmail.com>
2024-06-17 00:53:09 -0400

    used usestate for add task

--- ToDO-List/src/main.jsx
+++ ToDO-List/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './ToDoApp.jsx'
 import './index.css'
+import ToDoApp from './ToDoApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ToDoApp />
   </React.StrictMode>,
 )
